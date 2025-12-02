eb7e63d8c3fb6d88b75a009acf6172f7bbca88a1
d1ng1724 <s25004@gsm.hs.kr>
2025-12-02 23:39:44 +0900

    JWT 발급 초안, 시큐리티 설정 및 USER 엔티티 추가

---
+++ src/main/java/org/example/chain/ChainApplication.java
@@ -0,0 +1,13 @@
+package org.example.chain;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChainApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ChainApplication.class, args);
+    }
+
+}
---
+++ src/main/java/org/example/chain/domain/user/data/request/UpdateReq.java
@@ -0,0 +1,4 @@
+package org.example.chain.domain.user.data.request;
+
+public record ChangeReq(String email) {
+}
