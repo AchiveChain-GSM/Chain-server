32e1b233e0e7a91c6ba5f48ea8805dd4815aa138
d1ng1724 <s25004@gsm.hs.kr>
2025-12-19 11:03:38 +0900

    Mapping 어노테이션 추가

--- src/main/java/org/example/chain/domain/user/controller/UserController.java
+++ src/main/java/org/example/chain/domain/user/controller/UserController.java
@@ -15,20 +15,24 @@ import org.springframework.web.bind.annotation.*;
 public class UserController {
     private final UserService userService;
 
+    @PostMapping
     public ResponseEntity<HttpStatus> createUser (@RequestBody SignUpReq request){
         userService.createUser(request);
         return ResponseEntity.status(HttpStatus.CREATED).build();
     }
 
+    @GetMapping("/{id}")
     public ResponseEntity<UserRes> readUser (@PathVariable("id") Long id){
         return new ResponseEntity<>(userService.readUser(id), HttpStatus.OK);
     }
 
+    @PutMapping
     public ResponseEntity<HttpStatus> updateUser(@RequestBody UpdateReq request){
         userService.updateUser(request);
         return new ResponseEntity<>(HttpStatus.OK);
     }
 
+    @DeleteMapping("/{id}")
     public ResponseEntity<Void> deleteUser (@PathVariable("id") Long id){
         userService.deleteUser(id);
         return ResponseEntity.status(HttpStatus.NO_CONTENT).build();
