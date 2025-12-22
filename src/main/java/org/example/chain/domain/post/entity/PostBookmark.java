dd6a01b50a5e2802ed4079c21bf2d966da79e9d9
d1ng1724 <s25004@gsm.hs.kr>
2025-12-22 09:43:38 +0900

    엔티티 수정

--- src/main/java/org/example/chain/domain/post/entity/PostBookmark.java
+++ src/main/java/org/example/chain/domain/post/entity/PostBookmark.java
@@ -1,19 +1,34 @@
 package org.example.chain.domain.post.entity;
 
 import jakarta.persistence.*;
-import lombok.RequiredArgsConstructor;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
 import org.example.chain.domain.user.entity.User;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import java.time.Instant;
 
 @Entity
-@RequiredArgsConstructor
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@EntityListeners(AuditingEntityListener.class)
 public class PostBookmark {
     @Id
-    @GeneratedValue
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "postBookmark_id")
     private Long id;
 
     @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "postBookmark_id", nullable = false)
     private Post post;
 
+    @CreatedDate
+    @Column(name = "createAt", updatable = false)
+    private Instant createAt;
+
+    @JoinColumn(name = "user_id")
     @ManyToOne(fetch = FetchType.LAZY)
     private User user;
 
