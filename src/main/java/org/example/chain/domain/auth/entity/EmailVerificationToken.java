b4483406192288511d1fd1226bbf072f653f982e
d1ng1724 <s25004@gsm.hs.kr>
2025-12-22 15:16:02 +0900

    OnetoOne -> ManyToOne

--- src/main/java/org/example/chain/domain/auth/entity/EmailVerificationToken.java
+++ src/main/java/org/example/chain/domain/auth/entity/EmailVerificationToken.java
@@ -22,7 +22,7 @@ public class EmailVerificationToken {
     @Column(nullable = false, unique = true)
     private String token;
 
-    @OneToOne(fetch = FetchType.LAZY)
+    @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "user_id", nullable = false)
     private User user;
 
