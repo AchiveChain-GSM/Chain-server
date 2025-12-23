73795da209712f445cc4bfa144fae12c5cf2c9a6
d1ng1724 <s25004@gsm.hs.kr>
2025-12-23 12:15:58 +0900

    배포를 위한 설정

--- src/main/java/org/example/chain/global/security/config/SecurityConfig.java
+++ src/main/java/org/example/chain/global/security/config/SecurityConfig.java
@@ -37,7 +37,7 @@ public class SecurityConfig {
 
                 // HTTP 요청에 대한 접근 권한 설정
                 .authorizeHttpRequests(auth -> auth
-                        .requestMatchers("/api/auth/login", "api/auth/sign-up",
+                        .requestMatchers("/api/auth/login", "/api/auth/sign-up",
                                 "api/auth/send-email", "api/auth/verify-email").permitAll()
                         .requestMatchers(HttpMethod.DELETE, "/api/users/**").hasRole("ADMIN")
                         .anyRequest().authenticated()
