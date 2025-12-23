0641cfc0e9d8fa1cbd6b11e6ef88b381955fd424
d1ng1724 <s25004@gsm.hs.kr>
2025-12-23 11:50:41 +0900

    배포를 위한 설정

--- src/main/java/org/example/chain/global/security/config/SecurityConfig.java
+++ src/main/java/org/example/chain/global/security/config/SecurityConfig.java
@@ -23,11 +23,6 @@ public class SecurityConfig {
     private final JwtAuthFilter jwtAuthFilter;
 
     @Bean
-    public PasswordEncoder passwordEncoder() {
-        return new BCryptPasswordEncoder();
-    }
-
-    @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         http
                 .cors(cors -> cors.configurationSource(corsConfigurationSource()))
