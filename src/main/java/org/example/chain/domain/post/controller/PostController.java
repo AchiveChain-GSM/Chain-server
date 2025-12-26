6b5ca72606a6aa41be59872e7c2ee6413302d21d
d1ng1724 <s25004@gsm.hs.kr>
2025-12-27 00:25:42 +0900

    자료 생성, 수정, 삭제 log 추가

--- src/main/java/org/example/chain/domain/post/controller/PostController.java
+++ src/main/java/org/example/chain/domain/post/controller/PostController.java
@@ -1,6 +1,7 @@
 package org.example.chain.domain.post.controller;
 
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.example.chain.domain.post.data.req.PostCreateReq;
 import org.example.chain.domain.post.data.req.PostReportReq;
 import org.example.chain.domain.post.data.req.PostUpdateReq;
@@ -14,6 +15,7 @@ import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+@Slf4j
 @RestController
 @RequestMapping("api/posts")
 @RequiredArgsConstructor
@@ -23,7 +25,9 @@ public class PostController {
     //게시물 생성 페이지, 바로 조회할 수 있는 URL 반환
     @PostMapping(value = "/create", consumes = MediaType.MULTIPART_FORM_DATA_VALUE)
     public ResponseEntity<Void> createPost(@ModelAttribute PostCreateReq request){
+        log.info("자료 생성");
         Long postId = postService.createPost(request);
+        log.info("자료 생성 완료");
         return ResponseEntity.created(java.net.URI.create("/api/posts/" + postId)).build();
     }
 
@@ -32,7 +36,9 @@ public class PostController {
     public ResponseEntity<Void> updatePost(
             @ModelAttribute PostUpdateReq updateRequest // @RequestBody -> @ModelAttribute
     ){
+        log.info("자료 수정");
         postService.updatePost(updateRequest);
+        log.info("자료 수정 완료");
         return ResponseEntity.noContent().build();
     }
 
@@ -41,7 +47,9 @@ public class PostController {
     public ResponseEntity<Void> deletePost(
             @PathVariable Long post_id
     ){
+        log.info("자료 삭제");
         postService.deletePost(post_id);
+        log.info("자료 삭제 완료");
         return ResponseEntity.ok().build();
     }
 
