5c95ca6e400bc61d1cbc8bf072a130b84dde36ca
d1ng1724 <s25004@gsm.hs.kr>
2025-12-27 16:05:39 +0900

    댓글 작성 요청이후 바로 댓글을 찾을 수 있도록 url 반환

--- src/main/java/org/example/chain/domain/post/controller/PostCommentController.java
+++ src/main/java/org/example/chain/domain/post/controller/PostCommentController.java
@@ -2,10 +2,12 @@ package org.example.chain.domain.post.controller;
 
 import lombok.RequiredArgsConstructor;
 import org.example.chain.domain.post.data.req.PostCommentCreateReq;
+import org.example.chain.domain.post.data.res.PostCommentReadRes;
 import org.example.chain.domain.post.service.PostCommentService;
-import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 
 @RestController
@@ -16,12 +18,23 @@ public class PostCommentController {
 
     //게시물에, 댓글 생성 페이지
     @PostMapping
-    public ResponseEntity<Void> createComment(
+    public ResponseEntity<String> createComment(
             @PathVariable Long postId,
             @RequestBody PostCommentCreateReq request) {
 
+        String url = "/api/posts/" + postId + "comments";
+
         commentService.createComment(postId, request);
-        return ResponseEntity.status(HttpStatus.CREATED).build();
+        return ResponseEntity
+                .created(java.net.URI.create(url))
+                .body(url);
     }
 
+    @GetMapping
+    public ResponseEntity<List<PostCommentReadRes>> readComments(
+            @PathVariable Long postId
+    ) {
+        List<PostCommentReadRes> comments = commentService.readComments(postId);
+        return ResponseEntity.ok(comments);
+    }
 }
