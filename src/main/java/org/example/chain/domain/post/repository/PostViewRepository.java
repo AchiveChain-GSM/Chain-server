b48f1528e487b648e1c0c3f3cddb73a00973ea79
d1ng1724 <s25004@gsm.hs.kr>
2025-12-29 23:20:38 +0900

    안쓰는거지움

--- src/main/java/org/example/chain/domain/post/repository/PostViewRepository.java
+++ src/main/java/org/example/chain/domain/post/repository/PostViewRepository.java
@@ -24,8 +24,6 @@ public interface PostViewRepository extends JpaRepository<PostView, Long> {
     @Query("SELECT DISTINCT pv.post FROM PostView pv WHERE pv.user.id = :user_id ORDER BY pv.createAt DESC ")
     Page<Post> findAllPostsByUserIdSortRecentViewed(Pageable pageable, @Param("user_id") Long user_id);
 
-    Optional<PostView> findByPostAndUser(Post post, User user)
-
     Optional<PostView> findTopByPostAndUserAndCreatedAtAfterOrderByCreatedAtDesc(
             Post post,
             User user,
